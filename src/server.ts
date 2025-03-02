3b034ff58e194d91e3cd74d645cd0a30c1fd12b8
davideparrini <davide_parrini@libero.it>
2025-03-02 23:13:14 +0100

    commit solo server

---
+++ src/server.ts
@@ -0,0 +1,53 @@
+import express from "express";
+import http from "http";
+import { Server as SocketIOServer } from "socket.io";
+import cors from "cors";
+import { setIoInstance, setupSocketServer } from "./socket";
+import { errorMiddleware } from "./middlewares/errors-handler";
+import { logger } from "./middlewares/logger";
+import lobbyRouter from "./routes/lobby-routes";
+import gameRouter from "./routes/game-routes";
+import { API_PRIVATE, PORT } from "./Config/config";
+import { authenticationHandler } from "./middlewares/authentication-handler";
+import genericRouter from "./routes/generic-routes";
+
+
+process.on("uncaughtException", (err) => {
+  console.error("❌ Errore fatale non catturato:", err);
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("⚠️ Promise rifiutata senza gestione:", reason);
+});
+
+
+// Inizializzazione Express
+const app = express();
+const server = http.createServer(app);
+const io = new SocketIOServer(server, {
+  cors: { origin: "*" }
+});
+// Setup WebSocket
+setIoInstance(io);
+setupSocketServer(io);
+
+// Middleware
+app.use(logger);
+app.use(cors());
+app.use(express.json());
+app.use(API_PRIVATE, authenticationHandler);
+
+// Router
+app.use(`${API_PRIVATE}`, genericRouter);
+app.use(`${API_PRIVATE}/lobby`, lobbyRouter);
+app.use(`${API_PRIVATE}/game`, gameRouter);
+
+// Middleware gestione errori
+app.use(errorMiddleware);
+
+
+
+// Avvia il server
+server.listen(PORT, () => {
+  console.log(`Server in ascolto su ${PORT}`);
+});
